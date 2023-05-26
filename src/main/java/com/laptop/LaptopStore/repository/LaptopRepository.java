c89fc1f32a150cf0da77cf123725c74ca98e2633
RadmiloM <markovicradmilo35@gmail.com>
2023-05-26 19:07:31 +0200

    LaptopRepository created.

---
+++ src/main/java/com/laptop/LaptopStore/repository/LaptopRepository.java
@@ -0,0 +1,10 @@
+package com.laptop.LaptopStore.repository;
+
+import com.laptop.LaptopStore.entity.Laptop;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LaptopRepository extends JpaRepository<Laptop, Long> {
+
+}
